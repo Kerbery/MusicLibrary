4a8efc655bb113e020ecd205b0c8c1ebc26e78f9
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-09-09 15:17:41 +0300

    Introduced the fetching of user info from UserProvider to be displayed in Profile

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,31 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import LazyBackground from "../LazyBackground/LazyBackGround";
+import UserService from "../../services/user.service";
 import "./Profile.css";
 
-export default class Profile extends Component {
+export default class Profile extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
     this.state = {
       userId: this.props.user,
       page: this.props.page,
+      user: {},
+      isLoading: true,
     };
   }
 
+  componentDidMount() {
+    let { userId } = this.state;
+    this.fetchUserInfo(userId);
+  }
+
+  fetchUserInfo(userId) {
+    UserService.getUserInfo(userId).then((user) => {
+      this.setState({ user, isLoading: false });
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.user !== this.props.user) {
+      this.setState({ userId: this.props.user, isLoading: true }, () => {
+        this.fetchUserInfo(this.state.userId);
+      });
+    }
+  }
+
   render() {
-    const { userId } = this.state;
+    const { userId, isLoading } = this.state;
     const pages = {
       Profile: "",
       Tracks: "/tracks",
       Likes: "/likes",
       Playlists: "/playlists",
     };
+    let backgroundImage = this.state.user?.avatarUrl || "";
+    if (isLoading) {
+      return <div className="container" />;
+    }
     return (
       <div className="container">
-        <header className=" profileHeader d-flex">
-          <span className="profileHeaderAvatar"></span>
+        <header className="profileHeader d-flex">
+          <LazyBackground
+            src={backgroundImage}
+            alt=""
+            className="profileHeaderAvatar"
+          />
           <div className="align-self-start p-2">
-            <h2 className="profileTitle">{userId}</h2>
+            <h2 className="profileTitle">{this.state.user.userName}</h2>
+            <br />
+            {this.state.user.firstName && (
+              <h3 className="profileAdditionalInfo">
+                {`${this.state.user.firstName} ${this.state.user.lastName}`}
+              </h3>
+            )}
           </div>
         </header>
 
