58633c2fe29dfbe7a3e1ec72ced8083d23012c0c
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-09-21 17:45:46 +0300

    Added API calls to the LikedTracks endpoint

--- frontend/src/services/playlists.service.js
+++ frontend/src/services/playlists.service.js
@@ -69,5 +69,102 @@ class PlaylistService {
         (error) => console.log(error)
       );
   }
+
+  async getLikedTracksIds(userPermalink, PageNumber, PageSize) {
+    let token = await AuthService.getAccessToken();
+    let params = new URLSearchParams({
+      userPermalink,
+      PageNumber,
+      PageSize,
+    });
+    return axios
+      .get(`${LIKED_TRACKS_API_URL}/Ids?${params}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(
+        (response) => {
+          let paging = JSON.parse(response.headers["x-pagination"] || {});
+          let ids = response.data;
+          return { paging, ids };
+        },
+        (error) => {
+          debugger;
+          console.log(error);
+          if (error.response.status === 401) {
+            //AuthService.login();
+          }
+        }
+      );
+  }
+
+  async getAllLikedTracksIds(userPermalink) {
+    let ids = [];
+    let PageNumber = 1;
+    let PageSize = 200;
+
+    while (true) {
+      const result = await this.getLikedTracksIds(
+        userPermalink,
+        PageNumber,
+        PageSize
+      ).catch((error) => {
+        debugger;
+        console.log(error);
+      });
+
+      ids = [...ids, ...result.ids];
+
+      if (!result.paging.HasNext) {
+        break;
+      }
+
+      PageNumber += 1;
+    }
+
+    return ids;
+  }
+
+  async likeTrack(trackId) {
+    let token = await AuthService.getAccessToken();
+    return axios
+      .put(
+        `${LIKED_TRACKS_API_URL}/${trackId}`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then(
+        (response) => {
+          debugger;
+          console.log(response);
+          return response.status === 200;
+        },
+        (error) => {
+          debugger;
+          console.log(error);
+          return error.response.status === 200;
+        }
+      );
+  }
+
+  async unlikeTrack(trackId) {
+    let token = await AuthService.getAccessToken();
+    return axios
+      .delete(`${LIKED_TRACKS_API_URL}/${trackId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(
+        (response) => {
+          debugger;
+          console.log(response);
+          return response.status === 200;
+        },
+        (error) => {
+          console.log(error);
+          return error.response.status === 200;
+        }
+      );
+  }
 }
 export default new PlaylistService();
