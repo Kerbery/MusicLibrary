7e0486b5b3a6263d4c4d10111fe8ab60c8331ac1
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-10-02 23:58:55 +0300

    Updated mappers

--- frontend/src/services/mapping.helpers.js
+++ frontend/src/services/mapping.helpers.js
@@ -1,26 +1,52 @@
 export function getTrackData(track) {
-  var gridItemData = {};
-  gridItemData.title = track.title;
-  gridItemData.user = {
+  const user = {
     username: track.user.userName,
     permalink: track.user.permalink,
+    url: `/${track.user.permalink}`,
   };
 
-  gridItemData.artworkUrl = track.artworkUrl || "";
-  gridItemData.url = `/${track.user.permalink}/${track.permalink}`;
-  return gridItemData;
+  const trackData = {
+    title: track.title,
+    artworkUrl: track.artworkUrl || "",
+    url: `/${track.user.permalink}/${track.permalink}`,
+    user,
+  };
+  return trackData;
+}
+
+export function getTrackMediaData(track) {
+  const user = {
+    username: track.user.userName,
+    permalink: track.user.permalink,
+    avatarUrl: track.user.avatarUrl,
+    url: `/${track.user.permalink}`,
+  };
+
+  const trackData = {
+    title: track.title,
+    artworkUrl: track.artworkUrl || "",
+    url: `/${track.user.permalink}/${track.permalink}`,
+    mediaUrl: track.mediaUrl,
+    user,
+  };
+  return trackData;
 }
 
 export function getPlaylistData(playlist) {
-  var gridItemData = {};
-  gridItemData.title = playlist.title;
-  gridItemData.user = {
+  const user = {
     username: playlist.user.userName,
     permalink: playlist.user.permalink,
   };
-  gridItemData.artworkUrl = playlist.items?.[0]?.getTrackDTO?.artworkUrl || "";
-  gridItemData.url = `/${playlist.user.permalink}/playlists/${playlist.playlistId}`;
-  return gridItemData;
+
+  const playlistData = {
+    id: playlist.playlistId,
+    title: playlist.title,
+    artworkUrl: playlist.items?.[0]?.getTrackDTO?.artworkUrl || "",
+    url: `/${playlist.user.permalink}/playlists/${playlist.playlistId}`,
+    items: playlist.items,
+    user,
+  };
+  return playlistData;
 }
 
 export function getTrackDataFromItem(item) {
